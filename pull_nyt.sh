812a5e1928a048154992ca0bab8af5c808448cf0
Sejal Dua <sejal.dua@tufts.edu>
2020-05-03 10:04:29 -0700

    pull 5/2 data

--- pull_nyt.sh
+++ pull_nyt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../covid-19-data
 git pull
-cp us-counties.csv ../covid-movement-vis/data/cases-data/
-cd ../covid-movement-vis/data/cases-data
+cp us-counties.csv ../covid-tracker/data/cases-data/
+cd ../covid-tracker/data/cases-data
 python csv2json.py us-counties.csv old-us-counties-cases.json
 python gen_cases_data.py
 mv us-county-cases.json ../../
